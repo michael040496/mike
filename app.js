85d4981581023072bf5cb6d8ed7f3e17cb44ebf4
Michael <Michael.Dejene.Kifle@no.experis.com>
2019-03-12 10:20:54 +0100

    Initialise

--- app.js
+++ app.js
@@ -7,20 +7,39 @@ function pet(name, age, type){
     this.age = age;
     this.type = type;
 }
+   
 
-var data = new pet(
-    "Snoopy",
-    3,
-    "Dog");
+var ant = new pet(
+    "Anty",
+    5,
+    "Anteater");
+
+    var bunny = new pet(
+        "thugz bunny",
+        12,
+        "pimp");
+        
+        var data = [bunny, ant];
 
 app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/index.html'))
+});
+
+app.get('/info', (req, res) => {
     res.sendFile(path.join(__dirname + '/hello.html'))
+});
+
+app.get('/pictures', (req, res) => {
+    res.sendFile(path.join(__dirname + '/pictures.html'))
+
 });
 
 app.get('/data', (req, res) => {
     res.json(data)
 
 });
+
+
 
 app.listen(process.env.PORT || 8080)
 
